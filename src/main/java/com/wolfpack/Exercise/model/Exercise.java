81bc516eb4614f81d19edb157a2452e48c91b4f7
Devin Lewis <dglewis@umich.edu>
2023-01-22 15:25:18 -0500

    sql driver dependency, exercise model class

--- src/main/java/com/wolfpack/Exercise/model/Exercise.java
+++ src/main/java/com/wolfpack/Exercise/model/Exercise.java
@@ -1,4 +1,52 @@
 package com.wolfpack.Exercise.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.List;
+
+@Entity
 public class Exercise {
+    //PARAMETERS
+    @Id
+    private String name;
+    @Column
+    private List<String> muscles;
+    @Column
+    private String tutorial;
+
+    //CONSTRUCTORS
+    public Exercise() {}
+
+    public Exercise(String name, List<String> muscles, String tutorial) {
+        this.name = name;
+        this.muscles = muscles;
+        this.tutorial = tutorial;
+    }
+    //GETTERS AND SETTERS
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getMuscles() {
+        return muscles;
+    }
+
+    public void setMuscles(List<String> muscles) {
+        this.muscles = muscles;
+    }
+
+    public String getTutorial() {
+        return tutorial;
+    }
+
+    public void setTutorial(String tutorial) {
+        this.tutorial = tutorial;
+    }
+
 }
